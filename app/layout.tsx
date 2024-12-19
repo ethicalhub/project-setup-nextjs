1b8aee96de6103f0e6dbdb46941bb7c58a75dd6e
yasir-panther <yasir@pantherprotocol.io>
2024-12-19 19:56:52 +0530

    feat(setup): project Init with husky, commitlint, eslint, prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
 import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
 const geistSans = Geist({
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { dirname } from "path";
+import importPlugin from "eslint-plugin-import";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
@@ -10,7 +11,54 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ),
+  {
+    plugins: {
+      importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+
+              group: "external",
+
+              position: "after",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+
+          alphabetize: {
+            order: "asc",
+
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignores: ["components/ui/**"],
+
+    files: ["**/*.ts", "**/*.tsx"],
+  },
 ];
 
 export default eslintConfig;
